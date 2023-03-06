4052587111c28636c1eff130126673f2f6a5552e
israa burhan <israaburhan97@gmail.com>
2023-03-06 23:01:30 +0300

    Add Project Files

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import "../scss/app.scss";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+
+import "bootstrap/dist/js/bootstrap.bundle";
+
+const home = require("./home");
+document.open();
+document.write("<h1>mostafa</h1>");
+// document.getElementById("israa").innerHTML = "mos";
+document.close();
